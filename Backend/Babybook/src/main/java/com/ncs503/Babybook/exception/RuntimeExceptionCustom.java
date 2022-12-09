84a67ae6a36bed0f69e2cee6401f10b0d890f46c
Manuel Martinez <68873171+Manu-Martinez@users.noreply.github.com>
2022-12-09 15:30:29 -0300

    Deleted backend directory

--- Backend/Babybook/src/main/java/com/ncs503/Babybook/exception/RuntimeExceptionCustom.java
+++
@@ -1,9 +0,0 @@
-package com.ncs503.Babybook.exception;
-
-
-public class RuntimeExceptionCustom extends RuntimeException{
-    public RuntimeExceptionCustom(String msg) {
-        super(msg);
-    }
-}
-
